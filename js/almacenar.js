ee15dc36a9c311d9beec5ba63501158667ac0844
JulianCastill0 <juli.castillo909@gmail.com>
2023-08-21 11:35:39 -0300

    Trabajo hecho

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,11 +1,35 @@
-const btnAgregar = document.querySelector('#agregar');
-const itemInput = document.querySelector('#item').value;
+const btnAdd = document.getElementById("agregar");
+const btnClear = document.getElementById("limpiar");
+let listado = [];
 
-let listaItems = [];
+const listaCont = document.getElementById("contenedor");
 
-btnAgregar.addEventListener('click', () => {
-	// Se trae el arreglo en formato JSON del localStorage
-	listaItems = JSON.parse(localStorage.getItem('listitaKey'));
-	listaItems.push(itemInput);
-	localStorage.setItem('listitaKey', JSON.stringify(listaItems));
-});
+document.addEventListener("DOMContentLoaded", () => {
+    let listaDelLocal = JSON.parse(localStorage.getItem("lista"));
+
+    if (listaDelLocal != null){
+
+        for (let element of listaDelLocal){
+            listado.push(element);
+            let fila = document.createElement("li");
+            fila.innerHTML = element;
+            listaCont.appendChild(fila);
+        }
+    }
+
+    btnAdd.addEventListener("click", () => {
+        let data = document.getElementById("item");
+        listado.push(data.value);
+        localStorage.setItem("lista", JSON.stringify(listado));
+        let fila = document.createElement("li");
+        fila.innerHTML = listado[listado.length - 1];
+        listaCont.appendChild(fila);
+        data.value = "";
+    })
+
+    btnClear.addEventListener("click", () => {
+        listado = [];
+        localStorage.clear();
+        contenedor.innerHTML = ""
+    })
+})
