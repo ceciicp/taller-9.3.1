2f7e156f0e91de82f90350c758de9580018efb70
JulianCastill0 <juli.castillo909@gmail.com>
2023-08-21 21:44:54 -0300

    Optimize un cacho el codigo :p

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,35 +1,37 @@
-const btnAdd = document.getElementById("agregar");
-const btnClear = document.getElementById("limpiar");
-let listado = [];
-
-const listaCont = document.getElementById("contenedor");
-
 document.addEventListener("DOMContentLoaded", () => {
+    const btnAdd = document.getElementById("agregar");
+    const btnClear = document.getElementById("limpiar");
+    const listaCont = document.getElementById("contenedor");
+
+    let listado = [];
+
+    function listar(elemento){
+        let fila = document.createElement("li");
+        fila.innerHTML = elemento;
+        listaCont.appendChild(fila);
+    }
+
     let listaDelLocal = JSON.parse(localStorage.getItem("lista"));
-
     if (listaDelLocal != null){
-
         for (let element of listaDelLocal){
             listado.push(element);
-            let fila = document.createElement("li");
-            fila.innerHTML = element;
-            listaCont.appendChild(fila);
+            listar(element);
         }
     }
 
     btnAdd.addEventListener("click", () => {
-        let data = document.getElementById("item");
-        listado.push(data.value);
-        localStorage.setItem("lista", JSON.stringify(listado));
-        let fila = document.createElement("li");
-        fila.innerHTML = listado[listado.length - 1];
-        listaCont.appendChild(fila);
-        data.value = "";
+        let data = document.getElementById("item").value;
+        if (data != ""){
+            listado.push(data);
+            localStorage.setItem("lista", JSON.stringify(listado));
+            listar(data);
+            data = "";
+        }
     })
 
     btnClear.addEventListener("click", () => {
         listado = [];
         localStorage.clear();
-        contenedor.innerHTML = ""
+        contenedor.innerHTML = "";
     })
 })
